00aafaf4a5c06dad4727040715be8daa2f69ca5b
Amarnath Yadav <amar312696@gmail.com>
2023-06-24 20:53:50 +0530

    created react_app

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <p>
+          Hello WOrld
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
