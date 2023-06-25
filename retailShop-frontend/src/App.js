10e416df197808de8ee4c4656f3c2608da917990
Amarnath Yadav <amar312696@gmail.com>
2023-06-26 00:12:10 +0530

    side intro

--- retailShop-frontend/src/App.js
+++ retailShop-frontend/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
 import Navbar from './components/Navbar';
+import Sideintro from './components/Sideintro';
+import Background from './components/Background';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Navbar />
+      <Navbar />
+      <Sideintro />
+      {/* <Background/> */}
        
       </header>
     </div>
---
+++ retailShop-frontend/src/components/Background.js
@@ -0,0 +1,17 @@
+import "./Background.css"
+
+const Background =()=>{
+    return(
+        <div className="backdisplay">
+            <div className="toptriangle">
+                <svg width="823" height="301" viewBox="0 0 823 301" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M395.959 282.718C403.942 292.04 418.36 292.04 426.343 282.718L738.955 -82.3771C750.066 -95.3526 740.846 -115.385 723.764 -115.385H98.5387C81.4565 -115.385 72.2366 -95.3526 83.3469 -82.377L395.959 282.718Z" fill="#FFF3E7"/>
+                <path d="M396.343 114.953L83.7552 -249.811C72.915 -262.461 81.9031 -282 98.562 -282L723.738 -282C740.397 -282 749.385 -262.461 738.545 -249.811L425.957 114.953C418.174 124.035 404.126 124.035 396.343 114.953Z" fill="#FF8C00" stroke="#FF8C00"/>
+                </svg>
+            </div>
+            <div></div>
+        </div>
+    );
+}
+
+export default Background;
