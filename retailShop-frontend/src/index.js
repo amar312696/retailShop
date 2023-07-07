0643ec18159e1e8dceff7fc8ea8f4bee1901332b
Amarnath Yadav <amar312696@gmail.com>
2023-07-07 22:25:51 +0530

    firebase setup

--- retailShop-frontend/src/index.js
+++ retailShop-frontend/src/index.js
@@ -5,6 +5,25 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA9bk63YLwOzjt_LtIV8RjoYwiJ4UmSejg",
+  authDomain: "retailshop-64fa8.firebaseapp.com",
+  projectId: "retailshop-64fa8",
+  storageBucket: "retailshop-64fa8.appspot.com",
+  messagingSenderId: "859987616753",
+  appId: "1:859987616753:web:1ec2005c73a64ace1dda50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -18,3 +37,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export const auth = getAuth(app);
+export default app;
