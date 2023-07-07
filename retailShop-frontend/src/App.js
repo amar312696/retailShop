ec9085003e269b6f9ab06b126d0d4f4096dd1431
Amarnath Yadav <amar312696@gmail.com>
2023-07-07 23:37:07 +0530

    added login pages for both retailer and customer

--- retailShop-frontend/src/App.js
+++ retailShop-frontend/src/App.js
@@ -6,6 +6,9 @@ import Loginpage from './components/login/Loginpage';
 import Productpage from './components/productpage/Productpage';
 import Checkout from './components/checkout/Checkout';
 import Ritemlisting from './components/ritemlisting/Ritemslisting';
+import Signuppage from './components/signup/Signup';
+import SignuppageR from './components/signup/SignupR';
+import LoginpageR from './components/login/LoginpageR';
 import {Route,Routes} from "react-router";
 
 function App() {
@@ -15,7 +18,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage/>}/>
         <Route path="/prelogin" element={<Prelogin/>}/>
+        <Route path="/signup" element={<Signuppage/>}/>
+        <Route path="/signupr" element={<SignuppageR/>}/>
         <Route path="/login" element={<Loginpage/>}/>
+        <Route path="/loginr" element={<LoginpageR/>}/>
         <Route path="/products" element={<Productpage/>}/>
         <Route path="/checkout" element={<Checkout/>}/>
         <Route path="/itemlist" element={<Ritemlisting/>}/>
--- retailShop-frontend/src/components/prelogin/Mainpre.js
+++ retailShop-frontend/src/components/prelogin/Mainpre.js
@@ -1,10 +1,20 @@
 import "./Mainpre.css"
 import { useNavigate } from "react-router-dom";
+
 const Mainpre =()=>{
 
     const navigate=useNavigate();
     const loginClick=()=>{
         navigate("/login");
+    }
+    const signupC=()=>{
+        navigate("/signup")
+    }
+    const signupR=()=>{
+        navigate("/signupr")
+    }
+    const loginClickR=()=>{
+        navigate("/loginr");
     }
 
     return(
@@ -15,11 +25,11 @@ const Mainpre =()=>{
                 <p className="retailP">
                 The ultimate online retail shop app, connecting shoppers with their favorite brands and products effortlessly.
                 </p>
-                <div className="signUp">
+                <div className="signUp" onClick={signupR}>
                     Sign Up
                 </div>
                 <div className="signUpR">
-                    <p>Alerady have an account? <span className="loginR" onClick={loginClick}>Login</span> here</p>
+                    <p>Alerady have an account? <span className="loginR" onClick={loginClickR}>Login</span> here</p>
                 </div>
            </div>
            <div className="rightsec">
@@ -27,7 +37,7 @@ const Mainpre =()=>{
                 <p className="customerP">
                 Join millions of shoppers, explore products, enjoy exclusive offers, and elevate your online retail experience.
                 </p>
-                <div className="signUp">
+                <div className="signUp" onClick={signupC}>
                         Sign Up
                 </div>
                 <div className="signUpC">
