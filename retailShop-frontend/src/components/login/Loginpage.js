92b0a9f99c2a1f97c0edffd102805827c0e39b59
Amarnath Yadav <amar312696@gmail.com>
2023-07-08 01:07:17 +0530

    implemented firebase login signup

--- retailShop-frontend/src/components/login/Loginpage.js
+++ retailShop-frontend/src/components/login/Loginpage.js
@@ -1,5 +1,7 @@
 import "./Loginpage.css"
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 
 const Loginpage=()=>{
     const navigate=useNavigate();
@@ -9,8 +11,32 @@ const Loginpage=()=>{
     const homeClick=()=>{
         navigate("/");
     }
+
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("")
+    const [error,setError]=useState("");
+
+    const login= async (e)=>{
+        e.preventDefault();
+        try {
+            if(!email||!password){
+                setError("all fields are mandatory");
+                return;
+            }
+            await signInWithEmailAndPassword(getAuth(),email,password);
+            console.log("error checkup on login page js")
+            navigate("/");
+            
+        } catch (error) {
+            // console.log("Error")
+            setError(error.message);
+        }
+    }
+
+    
     return(
         <div className="logpage">
+            
             <div className="loginback">
                 <h1 className="retailShop" onClick={homeClick}><span style={{ color: '#0000FF'  }}>r</span><span style={{ color: '#7EB1EC' }}>etail</span><span style={{ color: '#0000FF' }}>S</span><span style={{ color: '#7EB1EC' }}>hop</span>.</h1>
             </div>
@@ -24,16 +50,18 @@ const Loginpage=()=>{
                         <div className="loginL">Login</div>
                         <form className="loginform">
                             <label>Email Address:<br/>
-                                <input type="email" />
+                                <input type="email" value={email} onChange={(e)=>{setEmail(e.target.value)}}/>
                             </label><br/>
                             <label>Password:<br/>
-                                <input type="Password" />
+                                <input type="Password" value={password} onChange={(e)=>{setPassword(e.target.value)}}/>
                             </label>
                             <br/>
-                            <button type="submit" className="loginsubmission">
+                            <button type="submit" className="loginsubmission" onClick={login}>
                                 Login
                             </button>
-                            
+                            <div className="error">
+                                *{error}
+                            </div>
                         </form>
                         <div className="furtherinfo">
                             <p>Not a member yet? Become a member <span className="signUpOnLogin" onClick={signUpClick}>Sign Up</span></p>
--- retailShop-frontend/src/components/signup/Signup.js
+++ retailShop-frontend/src/components/signup/Signup.js
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./Signup.css";
 import { useNavigate } from "react-router-dom";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const Signuppage=()=>{
     const navigate=useNavigate();
@@ -10,6 +12,36 @@ const Signuppage=()=>{
         navigate("/");
     }
     
+    const [email,setEmail]=useState("");
+    const [password,setPassword]=useState("")
+    const [reEnterPassword,setReEnterPassword]=useState("");
+    const [error,setError]=useState("");
+    
+    const signUp= async (e) =>{
+
+        e.preventDefault();
+        try {
+            if(!email||!password||!reEnterPassword){
+                setError("all fields are mandatory");
+                return;
+            }
+            if(password!==reEnterPassword){
+                // console.log("password mismatch");
+                setError("password mismatch");
+                return;
+            }
+
+            await createUserWithEmailAndPassword(getAuth(),email,password);
+            console.log("back to home");
+            navigate("/");
+        } catch (err) {
+                // console.log("error checkup on signup page js")
+                setError(err.message);
+        }
+    }
+
+
+
     return(
         <div className="signpage">
             <div className="signupback" onClick={homeClick}>
@@ -25,21 +57,24 @@ const Signuppage=()=>{
                         <div className="signupL">Sign Up</div>
                         <form className="signupform">
                             <label>Email Address:<br/>
-                                <input type="email" />
+                                <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                             </label><br/>
                             <label>Password:<br/>
-                                <input type="Password" />
+                                <input type="Password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
                             </label>
                             <br/>
                             <label>re-enter Password:<br/>
-                                <input type="Password" />
+                                <input type="Password" value={reEnterPassword} onChange={(e)=>setReEnterPassword(e.target.value)}/>
                             </label>
                             <br/>
-                            <button type="submit" className="loginsubmission">
+                            <button type="submit" className="loginsubmission" onClick={signUp}>
                                 Sign up
                             </button>
-                            
+                            <div className="error">
+                                *{error}
+                            </div>
                         </form>
+                        
                         <div className="furtherinfo">
                             <p>Already an existing member? Please <span className="signUpOnLogin" onClick={loginClick}>Login</span></p>
                         </div>
