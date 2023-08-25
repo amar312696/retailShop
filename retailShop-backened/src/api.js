660bfd8802e04cec5744416063488623c4797c23
Amarnath Yadav <amar312696@gmail.com>
2023-08-25 16:46:54 +0530

    some backend modification

--- retailShop-backened/src/api.js
+++ retailShop-backened/src/api.js
@@ -1,6 +1,18 @@
 import express from "express";
+import Product from "./schema.js";
+import { fileURLToPath } from 'url';
+import path from "path";
 
 const eapp=express();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+eapp.set('view engine', 'ejs');
+eapp.set('views', path.join(__dirname, '../views'));
+eapp.use(express.urlencoded({ extended: true }));
+eapp.use(express.static(path.join(__dirname, 'public')));
+eapp.use(express.json());
 
 eapp.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -8,8 +20,34 @@ eapp.use(function(req, res, next) {
     next();
   });
 eapp.get("/",(req,res)=>{
-    console.log(req)
+    // console.log(req)
     res.send("hello")
+})
+eapp.get("/product/:id",async(req,res)=>{
+  try {
+
+    const id=req.params.id;
+    const data=await Product.findById(id);
+    res.json(data);
+    
+  } catch (error) {
+    res.send(error.message);
+  }
+  
+})
+
+eapp.post("/product", async(req,res)=>{
+  try {
+    const data=req.body;
+    console.log(data);
+    const product=new Product(data);
+    await product.save();
+    res.status(200).send("success");
+    
+  } catch (error) {
+    res.send(error.message);
+  }
+    
 })
 
 
